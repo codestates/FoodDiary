6db0c67afae5a3f21df5666b90d6858e3890f188
Hyungjun <ryanromaris@naver.com>
2021-10-05 15:06:44 +0900

    Master: Add initial state

---
+++ FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/FoodDiaryServerApplication.java
@@ -0,0 +1,13 @@
+package Apoint.FoodDiary_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDiaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDiaryServerApplication.class, args);
+	}
+
+}
---
+++ FoodDiary_Server/src/test/java/Apoint/FoodDiary_Server/FoodDiaryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Apoint.FoodDiary_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDiaryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
