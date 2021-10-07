fe7c3806daeefa40b999b53be05964469f6a5545
Hyungjun <ryanromaris@naver.com>
2021-10-07 12:01:00 +0900

    Login:backup

--- FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Service/LoginService.java
+++ FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Service/LoginService.java
@@ -1,9 +1,13 @@
 package Apoint.FoodDiary_Server.Service;
 
 
+import Apoint.FoodDiary_Server.Domain.LoginSignin;
+import Apoint.FoodDiary_Server.Entity.ServiceUser;
 import Apoint.FoodDiary_Server.Repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LoginService {
@@ -14,5 +18,15 @@ public class LoginService {
     @Autowired
     public LoginService(LoginRepository loginRepository){
         this.loginRepository = loginRepository;
+    }
+
+    public ServiceUser FindUserData(LoginSignin loginSignin){
+        List<ServiceUser> user = loginRepository.FindByEmail(loginSignin.getEmail());
+        for (ServiceUser i : user){
+            if (i.getPassword().equals(loginSignin.getPassword())){
+                return i;
+            }
+        }
+        return null;
     }
 }
