6b5fb87fa77c1fb19c4cc8868defda71f667cc31
UnicusStella <unicusstella@gmail.com>
2021-10-07 14:08:53 +0900

    Signup: Add LoginController api function

--- FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Controller/LoginController.java
+++ FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Controller/LoginController.java
@@ -1,9 +1,16 @@
 package Apoint.FoodDiary_Server.Controller;
 
+import Apoint.FoodDiary_Server.Domain.LoginSignup;
 import Apoint.FoodDiary_Server.Service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin(origins = "https://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
@@ -14,8 +21,20 @@ public class LoginController {
     private final LoginService loginService;
 
     @Autowired
-    public LoginController(LoginService loginService){
+    public LoginController(LoginService loginService) {
         this.loginService = loginService;
+    }
+
+    @PostMapping(value = "/signup")
+    public ResponseEntity<?> UserSignUp(@RequestBody(required = true) LoginSignup loginSignup, HttpServletResponse response) {
+        if (loginSignup.getEmail() == null || loginSignup.getBirth() == null || loginSignup.getPassword() == null ||
+                loginSignup.getUsername() == null) {
+            return ResponseEntity.badRequest().body("insufficient parameters upplied");
+        }
+
+        return ResponseEntity.ok().body(new HashMap<>(){{
+            put("message","ok");
+        }});
     }
 
 
