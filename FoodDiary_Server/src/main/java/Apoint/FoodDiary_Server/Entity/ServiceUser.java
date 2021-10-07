bbc40d560fa7c81e64e8043425778dea0aaff96b
Hyungjun <ryanromaris@naver.com>
2021-10-07 11:10:31 +0900

    Login: Add initial state

--- FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Entity/ServiceUser.java
+++ FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Entity/ServiceUser.java
@@ -1,4 +1,99 @@
 package Apoint.FoodDiary_Server.Entity;
 
+import java.util.Date;
+import javax.persistence.*;
+
+
+@Entity
 public class ServiceUser {
+    @Id
+    private long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String birth;
+
+    @Column(nullable = false)
+    private String userImage;
+
+    @Column(nullable = false)
+    private Boolean admin;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    public ServiceUser(){ }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getBirth() { return birth; }
+
+    public void setBirth(String birth) { this.birth = birth; }
+
+    public String getUserImage() { return userImage; }
+
+    public void setUserImage(String userimage) { this.userImage = userImage; }
+
+    public Boolean getAdmin() { return admin; }
+
+    public void setAdmin(Boolean admin) { this.admin = admin; }
 }
--- FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Service/LoginService.java
+++ FoodDiary_Server/src/main/java/Apoint/FoodDiary_Server/Service/LoginService.java
@@ -1,4 +1,18 @@
 package Apoint.FoodDiary_Server.Service;
 
+
+import Apoint.FoodDiary_Server.Repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
+    private static long GET_ID = 0L;
+    private final static String SIGN_KEY = "apointkey";
+    private final LoginRepository loginRepository;
+
+    @Autowired
+    public LoginService(LoginRepository loginRepository){
+        this.loginRepository = loginRepository;
+    }
 }
