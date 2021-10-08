c774057992088145f5fb729e7a500d635a8659bc
sehan95 <sehan95@gmail.com>
2021-10-09 03:11:26 +0900

    login repaired

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -10,7 +10,7 @@ import SignIn from '../Components/SignIn';
 import SignUp from '../Components/Signup';
 
 
-function Login() {
+function Login({handleResponseSuccess}) {
   const [isLogin, setIsLogin] = useState(false);
   
 const changeLogin=() => {
@@ -20,8 +20,6 @@ const changeLogin=() => {
       setIsLogin(true);
     }   
   }
-
-
   return (
       <div>
         <Grid container>
@@ -39,7 +37,7 @@ const changeLogin=() => {
                   <div className="login_signin">
                     
                     {
-                      isLogin ? <SignIn/> : <SignUp changeLogin={changeLogin}/>
+                      isLogin ? <SignIn handleResponseSuccess={handleResponseSuccess}/> : <SignUp changeLogin={changeLogin}/>
                     }
                     
                     <div className="login_separatordiv">
